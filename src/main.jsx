31141c4f35143473dbe12b5aa443dfb7fd44f01d
Saju-7 <pokosoko052@gmail.com>
2025-02-18 15:18:45 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./style/flexboxgrid.min.css";
+import './style/index.css';
+import App from './App';
+import React from 'react';
+// Create a root element and render the app
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
